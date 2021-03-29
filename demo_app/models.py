dfb84ca41f32eaf3d630b638fe82bd05b69afdb0
Ryan Magley <ryan.magley@gmail.com>
2021-03-29 14:33:45 -0500

    Created ExampleModel in demo_app

--- demo_app/models.py
+++ demo_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ExampleModel(models.Model):
+
+    name = models.CharField(max_length = 50)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
