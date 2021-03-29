2a0b5551f197d521723e9e150dbf8a20c8f43b4a
Ryan Magley <ryan.magley@gmail.com>
2021-03-29 15:00:06 -0500

    Added route to add new ExampleModel

--- demo_app/views.py
+++ demo_app/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render, redirect, HttpResponse
+from .models import ExampleModel
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    context = {
+        'all_the_stuff_from_the_database': ExampleModel.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def add_new_thing(request):
+    # I am a comment
+    # I don't really do anything
+
+    ExampleModel.objects.create(
+        name = request.POST['name'] # this is important!!!
+    )
+
+    return redirect('/')
